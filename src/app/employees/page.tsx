521e83fbb81c512199f3e215c3c98b78af1d98d1
Yeminfu <yeminfu4319@gmail.com>
2023-11-15 15:01:26 +1000

    up

---
+++ src/app/employees/page.tsx
@@ -0,0 +1,9 @@
+import ViewAllEmployees from "@/components/employees/viewAll/ViewAllEmployees";
+
+export default async function PAGE() {
+    return <>
+        <h1>employeess</h1>
+        <ViewAllEmployees />
+        
+    </>
+}
---
+++ src/components/employees/viewAll/ViewAllEmployees.tsx
@@ -0,0 +1,16 @@
+import db_connection from "@/app/tools/dbConnect";
+
+export default async function ViewAllEmployees() {
+    const employees = await getAllEmployees();
+    return <>
+        <pre>
+            {JSON.stringify(employees, null, 2)}
+        </pre>
+    </>
+}
+
+async function getAllEmployees() {
+    return db_connection.promise().query("select * from employees").then(([x]) => {
+        return x;
+    })
+}
